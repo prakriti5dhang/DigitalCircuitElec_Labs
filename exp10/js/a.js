dfdc55cd196531391e6d71c8801732a86448fc01
prakriti5dhang <prakritidhang@gmail.com>
2021-08-30 18:07:48 +0530

    Add part 2 in exp 10

---
+++ exp10/js/a.js
@@ -0,0 +1,20 @@
+function  showDiv(str)
+{
+	if(str == '1')
+	{
+		document.getElementById("part1Div").style.display = '';
+		document.getElementById("part2Div").style.display = 'none';
+		document.getElementById("help2").style.display = 'none';
+		document.getElementById("help1").style.display = '';
+	}
+	else 
+	{
+		document.getElementById("part2Div").style.display = '';
+		document.getElementById("part1Div").style.display = 'none';
+		document.getElementById("help1").style.display = 'none';
+		document.getElementById("help2").style.display = '';
+		
+	}
+	
+	
+}
